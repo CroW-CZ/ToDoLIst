687b34096a0c6ed99a4725c0d35002cc9e0fcb0b
CroW-CZ <kangmunheon@gmail.com>
2018-02-25 12:11:16 +0900

    function : save, load

--- ToDo.js
+++ ToDo.js
@@ -6,6 +6,8 @@ var todoTemplate = document.getElementById('todoTemplate').innerHTML;
 button.addEventListener('click', addTodo);
 todoText.addEventListener('keyup', checkEnter);
 
+todoList.innerHTML = load();
+
 function checkEnter(event) {
   if(event.keyCode == 13){
     addTodo();
@@ -21,13 +23,27 @@ function addTodo() {
   newTodo.innerHTML += todoText.value;
   todoText.value = '';
   newTodo.id = 'added';
+  save();
 }
 
 function todoStateChange(checkbox) {
   if(checkbox.checked){
     checkbox.parentElement.id = 'done';
+    checkbox.outerHTML = '<input type="checkbox" id="checkbox" checked onchange="todoStateChange(this);">';
+    save();
   }
   else {
     checkbox.parentElement.id = 'todo';
+    checkbox.outerHTML = '<input type="checkbox" id="checkbox" unchecked onchange="todoStateChange(this);">';    
+    save();
   }
+}
+
+function save() {
+  var saveList = document.getElementById('todo').parentElement.innerHTML;
+  localStorage.setItem('ToDoList', saveList);
+}
+
+function load() {
+  return localStorage.getItem('ToDoList');
 }
