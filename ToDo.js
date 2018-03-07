a22aa5722276b1e6f8580d43731e85a9c778575f
CroW-CZ <kangmunheon@gmail.com>
2018-03-07 20:20:55 +0900

    수정
    리스트 하단에 추가 -> 리스트 상단에 추가
    저장, 불러오기 DOM -> data

--- ToDo.js
+++ ToDo.js
@@ -2,12 +2,15 @@ var todoText = document.getElementById('textbox');
 var button = document.getElementById('button');
 var todoList = document.getElementById('list');
 var todoTemplate = document.getElementById('todoTemplate').innerHTML;
+var todo = [];
+var done = [];
 
 button.addEventListener('click', addTodo);
 todoText.addEventListener('keyup', checkEnter);
 todoList.addEventListener('change', todoStateChange);
 
-todoList.innerHTML = load();
+load();
+displayList();
 
 function checkEnter(event) {
   if(event.keyCode == 13){
@@ -19,11 +22,9 @@ function addTodo() {
   if (todoText.value == '') {
     return;
   }
-  todoList.innerHTML += todoTemplate;
-  var newTodo = document.getElementById('add');
-  newTodo.innerHTML += todoText.value;
+  todo.unshift(todoText.value);
   todoText.value = '';
-  newTodo.removeAttribute('id');
+  displayList();
   save();
 }
 
@@ -42,9 +43,35 @@ function todoStateChange(event) {
 }
 
 function save() {
-  localStorage.setItem('ToDoList', todoList.innerHTML);
+  //localStorage.setItem('ToDoList', todoList.innerHTML);
+  localStorage.setItem('ToDo', JSON.stringify(todo));
+  localStorage.setItem('Done', JSON.stringify(done));
 }
 
 function load() {
-  return localStorage.getItem('ToDoList');
+  //return localStorage.getItem('ToDo');.
+  todo = JSON.parse(localStorage.getItem('ToDo'));
+  done = JSON.parse(localStorage.getItem('Done'));
+  if (todo == null){
+    todo = [];
+  }
+  if (done == null){
+    done = [];
+  }
+}
+
+function displayList() {
+  todoList.innerHTML = '';
+  for (var i = 0; i < todo.length; i++){
+    todoList.innerHTML += todoTemplate;
+    var newTodo = document.getElementById('add');
+    newTodo.innerHTML += todo[i];
+    newTodo.removeAttribute('id');
+  }
+  for (var i = 0; i < done.length; i++){
+    todoList.innerHTML += todoTemplate;
+    var newTodo = document.getElementById('add');
+    newTodo.innerHTML += done[i];
+    newTodo.removeAttribute('id');
+  }
 }
